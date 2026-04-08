1e113d46b290b721222305c7acde3b5234e9b974
dacodermkd-cell <dacodermkd@gmail.com>
2026-04-09 04:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 )
 
 # ── Constants ──────────────────────────────────────────
-DATASET_URL = "PASTE_GDRIVE_FILE_ID_HERE"
+DATASET_URL = "https://drive.google.com/file/d/1H8A-jQZ64uPNgsXFurG6iY8f1MjIRrby/view?usp=sharing"
 ANNOTATIONS_FILE = "annotations.csv"
 DATASET_FILE = "annotation_dataset.json"
 
