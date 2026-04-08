7f6b504f007494a5f863d53bd2f0ec60c7d80ce1
dacodermkd-cell <dacodermkd@gmail.com>
2026-04-09 04:54:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 )
 
 # ── Constants ──────────────────────────────────────────
-DATASET_URL = "1H8A-jQZ64uPNgsXFurG6iY8f1MjIRrby/view?usp=sharing"
+DATASET_URL = "1H8A-jQZ64uPNgsXFurG6iY8f1MjIRrby"
 ANNOTATIONS_FILE = "annotations.csv"
 DATASET_FILE = "annotation_dataset.json"
 
